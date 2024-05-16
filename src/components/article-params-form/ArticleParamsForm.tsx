142fedb06168995d750ba0fc411b17b64c270948
Orit Gegelia <o.gegelia@yandex.ru>
2024-05-16 17:04:53 +0300

    First review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,135 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Text } from '../text';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState, useRef, useEffect } from 'react';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Separator } from '../separator';
 
-export const ArticleParamsForm = () => {
+type OnSubmit = (newValues: string[]) => void;
+interface ChildProps {
+	onSubmit: OnSubmit;
+}
+
+export const ArticleParamsForm = ({ onSubmit }: ChildProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [isSelected, setSelected] = useState({
+		font: fontFamilyOptions[0],
+		size: fontSizeOptions[0],
+		fontColor: fontColors[0],
+		bgColor: backgroundColors[0],
+		width: contentWidthArr[0],
+	});
+
+	const formRef = useRef<HTMLFormElement>(null);
+
+	const changeOpenState = () => {
+		setIsOpen(!isOpen);
+	};
+
+	useEffect(() => {
+		const closeByOverlay = (e: MouseEvent) => {
+			if (isOpen && !formRef.current?.contains(e.target as Node)) {
+				changeOpenState();
+			}
+		};
+
+		window.addEventListener('mousedown', closeByOverlay);
+
+		if (!isOpen) {
+			return () => {
+				window.removeEventListener('mousedown', closeByOverlay);
+			};
+		}
+	}, []);
+
+	const handleSelectChange = (
+		optionType: string,
+		selectedOption: OptionType
+	) => {
+		setSelected((prevOptions) => ({
+			...prevOptions,
+			[optionType]: selectedOption,
+		}));
+	};
+
+	const handleSettingsReset = () => {
+		setSelected({
+			font: fontFamilyOptions[0],
+			size: fontSizeOptions[0],
+			fontColor: fontColors[0],
+			bgColor: backgroundColors[0],
+			width: contentWidthArr[0],
+		});
+	};
+
+	// const { font, size, fontColor, bgColor, width } = isSelected;
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onSubmit(Object.values(isSelected).map((option) => option.value));
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={changeOpenState} isOpen={isOpen} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
+				<form ref={formRef} className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h2' weight={800} size={25} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						placeholder='Выберите шрифт'
+						options={fontFamilyOptions}
+						selected={isSelected.font}
+						onChange={(font) => handleSelectChange('font', font)}
+					/>
+					<RadioGroup
+						title={'Размер шрифта'}
+						name={'str'}
+						options={fontSizeOptions}
+						selected={isSelected.size}
+						onChange={(size) => handleSelectChange('size', size)}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={isSelected.fontColor}
+						onChange={(fontColor) => handleSelectChange('fontColor', fontColor)}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={isSelected.bgColor}
+						onChange={(bgColor) => handleSelectChange('bgColor', bgColor)}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={isSelected.width}
+						onChange={(width) => handleSelectChange('width', width)}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button
+							title='Сбросить'
+							type='submit'
+							onClick={handleSettingsReset}
+						/>
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,19 +13,29 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [newValues, setNewValues] = useState<string[]>([]);
+
+	const handleChangeValues = (newValues: string[]) => {
+		setNewValues(newValues);
+	};
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family':
+						newValues[0] || defaultArticleState.fontFamilyOption.value,
+					'--font-size':
+						newValues[1] || defaultArticleState.fontSizeOption.value,
+					'--font-color': newValues[2] || defaultArticleState.fontColor.value,
+					'--container-width':
+						newValues[4] || defaultArticleState.contentWidth.value,
+					'--bg-color':
+						newValues[3] || defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onSubmit={handleChangeValues} />
 			<Article />
 		</div>
 	);
